c869b2fbbf2815b20a605c692a808a1e00880bec
Juno Vue <Juno.Vue@Hotmail.com>
2019-04-05 18:21:32 -0500

    added connect(), mapRedux to props into export default to get dispatch to work

--- src/components/WebPages/Feeling/Feeling.js
+++ src/components/WebPages/Feeling/Feeling.js
@@ -1,13 +1,41 @@
 import React, { Component } from 'react';
 import '../Feeling/Feeling.css'
+import {withRouter} from 'react-router-dom';
+import { connect } from 'react-redux';
 
 class Feeling extends Component {
 
-    returnToPrevious = () => {
+    state = {
+        feedback: {
+            feeling: '',
+        },
+    }
+
+    returnToPrevious = (event) => {
         alert('You are going back to home!');
         // change location here 
         this.props.history.push('/'); 
     }
+
+    nextPage = (event) => {
+        event.preventDefault();
+        console.log('Button clicked', this.state.feedback);
+        const action= {type: 'FEELING', payload: this.state.feedback}
+        this.props.dispatch(action)
+    }
+
+    handleChange = (event) => {
+        let propertyName = event.target.name;
+        console.log(propertyName);
+        
+        this.setState({
+            feedback: {
+                ...this.state.feedback,
+                [propertyName]: event.target.value,
+            },
+          })
+        }
+
 
     render() {
         return (
@@ -21,11 +49,16 @@ class Feeling extends Component {
             </div>
             <div>
                 <button id="fixed-button" onClick={this.returnToPrevious}>Go back to home</button>
-                <button id="next-button" onClick={this.returnToPrevious}>Next Page</button>
+                <button id="next-button" onClick={this.nextPage} onChange={this.handleChange} > Next Page </button>
             </div>
         </section>
         );
     }
 }
 
-export default Feeling;
+const mapReduxStateToProps = (reduxState) => ({
+    reduxState,
+});
+
+export default connect(mapReduxStateToProps)(withRouter(Feeling));
+
--- src/index.js
+++ src/index.js
@@ -4,5 +4,43 @@ import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Redux
+import { createStore, /* combineReducers, */ applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+
+const feedback = {
+        feeling: '',
+        understanding: '',
+        support: '',
+        comments: '',
+        flagged: true,
+        date: '',
+}
+
+const feedbackInfo = (state = feedback, action ) => {
+    switch (action.type) {
+        case 'FEEDBACK': {
+          return {
+            ...state,
+            feeling: action.payload.feeling,
+          }
+        }
+        default: return state;
+    }
+}
+
+// The store is the big JavaScript Object that holds all of the information for our application
+const storeInstance = createStore(
+        feedbackInfo,
+    applyMiddleware(logger),
+); 
+
+// Wrap our App in a Provider, this makes Redux available in
+// our entire application
+ReactDOM.render(
+<Provider store={storeInstance}>
+<App />
+</Provider>, document.getElementById('root'));
 registerServiceWorker();
+
